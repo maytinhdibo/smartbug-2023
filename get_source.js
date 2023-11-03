0858a868e24b951e2048ef4758a38f95fa03511c
Cuong Tran <iammaytinhdibo@gmail.com>
2023-11-03 16:13:58 +0700

    add multi

--- get_source.js
+++ get_source.js
@@ -2,33 +2,39 @@ import fetch from 'node-fetch';
 import fs from 'fs';
 import cliProgress from 'cli-progress';
 import token from './token.json' assert { type: "json" };
+import { open } from 'node:fs/promises';
+
 
 
 var count = 0;
-const addresses = fs.readFileSync('all_contract.csv').toString().split("\n");
+// const addresses = fs.readFileSync('all_contract.csv').toString().split("\n");
 
 const bar = new cliProgress.SingleBar({
     format: '{percentage}%|{bar}| {value}/{total} [{duration_formatted}/ETA: {eta_formatted}]'
 }, cliProgress.Presets.shades_classic);
-bar.start(addresses.length, 0);
+bar.start(20000000, 0);
 
-for (const address of addresses) {
-    // const address = addresses[i];
-    const response = await fetch(`https://api.etherscan.io/api?module=contract&action=getsourcecode&address=`
-        + address
-        + `&apikey=`
-        + token.token);
-    const data = await response.json();
+const file = await open('./all_contract_1.csv');
+    for await (const address of file.readLines()) {
+    try{
+        const response = await fetch(`https://api.etherscan.io/api?module=contract&action=getsourcecode&address=`
+            + address
+            + `&apikey=`
+            + token.token);
+        const data = await response.json();
 
-    if (data.message.toUpperCase() == "OK") {
-        if(data.result[0].SourceCode.length>0){
-            fs.writeFileSync("sources/" + address + ".sol", data.result[0].SourceCode);
-        }else{
-            fs.writeFileSync("error/null_source.csv", address + "\n", { flag: "a+" })
+        if (data.message.toUpperCase() == "OK") {
+            if(data.result[0].SourceCode.length > 0){
+                fs.writeFileSync("sources/" + address + ".sol", data.result[0].SourceCode);
+            }else{
+                fs.writeFileSync("error/null_source.csv", address + "\n", { flag: "a+" })
+            }
+        } else {
+            fs.writeFileSync("sources/" + address + ".txt", JSON.stringify(data));
+            fs.writeFileSync("error/error.csv", address + "\n========\n", { flag: "a+" })
         }
-    } else {
-        fs.writeFileSync("sources/" + address + ".txt", JSON.stringify(data));
-        fs.writeFileSync("error/error.csv", address + "\n", { flag: "a+" })
+        bar.update(count++);
+    } catch (error) {
+        fs.writeFileSync("error/error.csv", address + "\n"+ error +"\n========\n", { flag: "a+" })
     }
-    bar.update(count++);
 }
